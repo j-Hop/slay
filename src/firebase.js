5e6eb86eb395ae1e859f25c3efbb641e4b897312
Tetsu <119629763+j-Hop@users.noreply.github.com>
2024-04-20 18:21:50 +0300

    update

--- src/firebase.js
+++ src/firebase.js
@@ -10,7 +10,6 @@ const {
   VITE_STORAGE_BUCKET,
   VITE_MESSAGING_SENDER_ID,
   VITE_APP_ID,
-  VITE_MEASUREMENT_ID,
 } = import.meta.env;
 
 const firebaseConfig = {
@@ -21,7 +20,6 @@ const firebaseConfig = {
   storageBucket: VITE_STORAGE_BUCKET,
   messagingSenderId: VITE_MESSAGING_SENDER_ID,
   appId: VITE_APP_ID,
-  measurementId: VITE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
