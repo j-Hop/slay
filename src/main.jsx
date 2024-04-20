496590c2c8ca34a9cbeb91503ae52d08b5d942a1
Tetsu <119629763+j-Hop@users.noreply.github.com>
2024-04-20 20:00:02 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -14,7 +14,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
       <ThemeProvider theme={theme}>
-        <BrowserRouter basename="/dual">
+        <BrowserRouter basename="/slay">
           <App />
           <Toaster
             toastOptions={{
