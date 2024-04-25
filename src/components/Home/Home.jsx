c144106075d8b0ac5967bf4a14c1d32b08d9d168
Tetsu <119629763+j-Hop@users.noreply.github.com>
2024-04-25 23:48:58 +0300

    up

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,5 +1,5 @@
 import SpriteIcon from '../../images/sprite.svg';
-import HeroImg from '../../images/Rectangle 1.png';
+import HeroImg from '../../images/child.png';
 import {
   BtnStart,
   BoxCheck,
